d172ff035ee74973588096b05d1a6e4d236dd87e
Angel Jesus Cabezas Vasquez <angelcabezasvasquez@gmail.com>
2026-06-28 16:51:19 -0500

    Dia 6: Menu interactivo completo

--- Codigo/main.cpp
+++ Codigo/main.cpp
@@ -4,52 +4,82 @@
 using namespace std;
 
 int main() {
-    cout << "SIMULADOR DE LA LEY DE HUBBLE" << endl;
-
-    cout << "Prueba de la clase Galaxia" << endl;
-    Galaxia g1("Andromeda", 0.78);
-    g1.calcularVelocidad(70.0);
-    g1.calcularDesplazamientoAlRojo();
-    g1.mostrar();
+    const double HUBBLE_CONSTANT = 70.0;
+    Universo universo(HUBBLE_CONSTANT);
     
-    cout << endl;
-
-    cout << "Prueba de la clase Universo" << endl;
-    Universo universo(70.0); 
-    cout << "Universo creado exitosamente." << endl;
-    cout << "Constante de Hubble: " << universo.getH0() << " km/s/Mpc" << endl;
+    cout << "=========================================" << endl;
+    cout << "    SIMULADOR DE LA LEY DE HUBBLE" << endl;
+    cout << "=========================================" << endl;
+    cout << "Constante de Hubble: " << HUBBLE_CONSTANT << " km/s/Mpc" << endl << endl;
     
-
-    cout << "Prueba de funcionagregarGalaxia() de la clase universo" << endl;
-    Galaxia g2("Via Lactea", 0.0);
-    universo.agregarGalaxia(g2);
-    cout << "Galaxia agregada al catalogo." << endl;
-    cout << "Total de galaxias: " << universo.getTotal() << endl;
-    
-
-    cout << "Prueba de funcion cargarCatalogo()" << endl;
+    // Cargar el catalogo automaticamente al iniciar
     universo.cargarCatalogo("datos/catalogo.csv");
-    cout << "Total de galaxias en el catalogo: " << universo.getTotal() << endl;
-
-    cout << "Prueba de funcion guardarResultados()" << endl;
-    universo.guardarResultados("datos/resultados.csv");
+    cout << "Catalogo cargado. " << universo.getTotal() << " galaxias encontradas." << endl << endl;
     
-    cout << "Prueba de ordenamiento por distancia" << endl;
-    universo.ordenarPorDistancia();
-    universo.mostrarCatalogo();
-
-    cout << "Prueba de busqueda por nombre" << endl;
-    int pos = universo.buscarPorNombre("Andromeda");
-    if (pos != -1) {
-        cout << "Galaxia encontrada:" << endl;
-        universo.mostrarCatalogo();         
-    } else {
-        cout << "Galaxia no encontrada." << endl;
-    }
-
-    cout << "Prueba de estadisticas" << endl;
-    universo.mostrarEstadisticas();
-
+    int opcion;
+    do {
+        cout << "MENU PRINCIPAL" << endl;
+        cout << "1. Mostrar todas las galaxias" << endl;
+        cout << "2. Ordenar por distancia (burbuja)" << endl;
+        cout << "3. Ordenar por velocidad (burbuja)" << endl;
+        cout << "4. Buscar galaxia por nombre" << endl;
+        cout << "5. Mostrar estadisticas" << endl;
+        cout << "6. Guardar resultados en archivo" << endl;
+        cout << "7. Salir" << endl;
+        cout << "Opcion: ";
+        cin >> opcion;
+        
+        switch (opcion) {
+            case 1:
+                cout << "\nCATALOGO DE GALAXIAS" << endl;
+                universo.mostrarCatalogo();
+                break;
+                
+            case 2:
+                cout << "\nORDENANDO POR DISTANCIA" << endl;
+                universo.ordenarPorDistancia();
+                universo.mostrarCatalogo();
+                break;
+                
+            case 3:
+                cout << "\nORDENANDO POR VELOCIDAD" << endl;
+                universo.ordenarPorVelocidad();
+                universo.mostrarCatalogo();
+                break;
+                
+            case 4: {
+                cout << "\nBUSCAR GALAXIA" << endl;
+                string nombre;
+                cout << "Ingrese el nombre de la galaxia: ";
+                cin >> nombre;
+                int pos = universo.buscarPorNombre(nombre);
+                if (pos != -1) {
+                    cout << "Galaxia encontrada:" << endl;
+                    universo.mostrarCatalogo();
+                } else {
+                    cout << "Galaxia no encontrada." << endl;
+                }
+                break;
+            }
+                
+            case 5:
+                cout << "\nESTADISTICAS" << endl;
+                universo.mostrarEstadisticas();
+                break;
+                
+            case 6:
+                cout << "\nGUARDANDO RESULTADOS" << endl;
+                universo.guardarResultados("datos/resultados.csv");
+                break;
+                
+            case 7:
+                cout << "\nSaliendo del simulador..." << endl;
+                break;
+                
+            default:
+                cout << "Opcion invalida. Intente nuevamente." << endl;
+        }
+    } while (opcion != 7);
+    
     return 0;
 }
-
